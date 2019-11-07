4a2d3602a2d3136f29a0d92a7e3e4b0f711b05a6
jimbits <contactjamesfoley@gmail.com>
2019-11-06 17:49:54 -0700

    javascript mobile menu

---
+++ jargon-v-09/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head();?>
+</head>
+
+
+<body <?php body_class(); ?>>
+
+<nav class="site-nav">
+       <ul class="icon-view">
+       <li>
+       <a class="home-link"  href="<?php bloginfo('url'); ?>">
+        <img class="mobile-menu-icon" src="http://jimf.dmitstudent.ca/dmit2032/section-one/wp-content/uploads/2019/10/logo-3.svg" alt="jargon company logo">
+        argon
+       </a>
+       </li> 
+             
+    
+  
+       <li >
+        <a class="login-button" href="http://jimf.dmitstudent.ca/dmit2032/section-one/wp-login">login</a>
+       </li>
+       
+       </ul>
+</nav>
+
+
+
+   
+
+    
+ 
